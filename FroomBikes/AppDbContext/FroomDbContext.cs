307fee9ee3caa884d9f75c5e345c00beb4b0d79b
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2020-07-19 21:34:47 +0100

    Added DbContext, Added Connectionstrings in appsettings.json, Created Migrationsa and Updated Database

---
+++ FroomBikes/AppDbContext/FroomDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FroomBikes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FroomBikes.AppDbContext
+{
+    public class FroomDbContext: DbContext
+    {
+        public FroomDbContext(DbContextOptions<FroomDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+    }
+}
